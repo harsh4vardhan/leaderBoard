7a0f03328bb91aa7995c75cb1a8bfe55cbb49b6f
root <root@Harsh>
2024-08-04 17:09:50 +0530

    compile files

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000', // Default to localhost for development
+  baseURL: import.meta.env.VITE_API_URL || 'http://89.116.159.2/', // Default to localhost for development
 });
 
 export default instance;
