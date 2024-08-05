03212f085c11c0440bf331bfe9d262f8ed7e912e
root <root@Harsh>
2024-08-05 12:04:25 +0530

    fix indentation of sort button

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="flex flex-col md:flex-row items-center p-4 bg-gray-100 border-b border-gray-200">
     <!-- Sort Buttons with Icons -->
-    <div class="flex flex-wrap space-x-2 mb-2 md:mb-2">
+    <div class="flex space-x-2 mb-2 md:mb-2">
       <button
         @click="sortByName"
         class="flex items-center bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 text-sm md:text-base"
       >
-        <i class="fas fa-sort-alpha-up mr-2 md:mr-3"></i>
         <span >Sort by Name</span>
       </button>
       <button
         @click="sortByPoints"
         class="flex items-center bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 text-sm md:text-base"
       >
-        <i class="fas fa-sort-numeric-up mr-2 md:mr-3"></i>
         <span >Sort by Points</span>
       </button>
     </div>
